0b080e8ca4eef9cd82a3ca0542005f3654a58bca
pjh <uji1208@snu.ac.kr>
2026-09-26 17:15:46 +0900

    fix : 프론트엔드 일정 및 행사 상세보기, 새로운 일정 및 행사 탐색 시 onClick 추가

--- frontend/src/components/list/EventsList.jsx
+++ frontend/src/components/list/EventsList.jsx
@@ -1,13 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 import EventItem from "../item/EventItem";
 import '../../styles/myPage.css'
 
 const EventsList = ({ ary, onUpdate}) => {
+    const moveUrl = useNavigate();
+
     if (ary.length === 0) {
         return (
             <div className="empty-state">
                 <p className="empty-state-title">아직 관심 있는 행사가 없어요.</p>
                 <p>마음에 드는 행사를 저장해보세요.</p>
-                <button className="primary-button">행사 둘러보기</button>
+                <button className="primary-button" onClick={() => moveUrl(`/events/${event.eventId}`)}>행사 둘러보기</button>
             </div>
         )
     }
